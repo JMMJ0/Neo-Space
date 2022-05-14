afbb8c24793d33d88adb008e921a251b281d248b
JMMJ0 <josemanuel.moron.alu@iescampanillas.com>
2022-05-14 18:02:54 +0200

    Destinos agregados

--- resources/views/destinos.blade.php
+++ resources/views/destinos.blade.php
@@ -9,28 +9,7 @@
   </head>
 
   <body>
-    <header>
-      <div class="container">
-        <nav id="navigation">
-          <a href="#" class="logo">
-            <div><img src="/svg/logoWhite.svg" alt="SpaceNeo" /></div>
-          </a>
-          <a aria-label="mobile menu" class="nav-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-          </a>
-          <ul class="menu-left">
-            <li><a href="/navbar.html">Inicio</a></li>
-            <li><a href="cohete.html">Cohetes</a></li>
-            <li><a href="#compania">Compañias</a></li>
-            <li><a href="#destino">Destinos</a></li>
-            <li><a href="#lohin">Log In</a></li>
-            <li><a href="#singup">Registrarse</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+   <x-nav/>
 
     <div class="tierra">
       <div class="msgs-diplayed1">
